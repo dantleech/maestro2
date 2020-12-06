f71d7d9d6cae4b3d530a81cda17d158240c10d1a
Daniel Leech <dan.t.leech@gmail.com>
2020-12-06 11:57:46 +0000

    Updated example

--- example/pipeline/PhpactorRepositoryPipeline.php
+++ example/pipeline/PhpactorRepositoryPipeline.php
@@ -5,6 +5,7 @@ namespace Maestro2\Examples\Pipeline;
 use Maestro2\Core\Config\RepositoryNode;
 use Maestro2\Core\Pipeline\RepositoryPipeline;
 use Maestro2\Core\Task\CommandsTask;
+use Maestro2\Core\Task\FileTask;
 use Maestro2\Core\Task\GitRepositoryTask;
 use Maestro2\Core\Task\SequentialTask;
 use Maestro2\Core\Task\Task;
@@ -15,15 +16,45 @@ class PhpactorRepositoryPipeline implements RepositoryPipeline
     public function build(RepositoryNode $repository): Task
     {
         return new SequentialTask([
+            new FileTask(
+                path: '.travis.yml',
+                exists: false
+            ),
             new TemplateTask(
+                group: $repository->name(),
                 template: 'example/pipeline/template/ci.yml.twig',
                 target: $repository->path('.github/workflows/ci.yml'),
+                overwrite: true,
+                vars: [
+                    'name' => $repository->name(),
+                    'jobs' => [
+                        //'composer',
+                        'php-cs-fixer',
+                        'phpstan',
+                        'tests',
+                    ],
+                    'phpVersions' => [
+                        '7.3',
+                        '7.4',
+                    ]
+                ]
             ),
-            $this->buildCi($repository)
+            new CommandsTask(
+                group: $repository->name(),
+                commands: [
+                    [ 'git',  'checkout',  '-b', 'githubactions' ],
+                    [ 'git',  'rm',  '.travis.yml' ],
+                    [ 'git',  'add',  '.github' ],
+                    [ 'git',  'commit',  '-m', 'Add github actions' ],
+                    [ 'git',  'push',  'origin', 'HEAD', '-f' ],
+                ],
+                cwd: $repository->path(),
+                failFast: true
+            ),
         ]);
     }
 
-    private function buildCi(RepositoryNode $repository)
+    private function buildCi(RepositoryNode $repository): Task
     {
         return new SequentialTask(array_map(function (string $phpVersion) use ($repository) {
             return new CommandsTask(
