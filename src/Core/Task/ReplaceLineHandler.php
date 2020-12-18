d83e7b6cfe2f0403baa26bf8b210701dd8e8f762
Daniel Leech <dan.t.leech@gmail.com>
2020-12-18 08:46:25 +0000

    Fixed replace-line

--- src/Core/Task/ReplaceLineHandler.php
+++ src/Core/Task/ReplaceLineHandler.php
@@ -4,6 +4,7 @@ namespace Maestro2\Core\Task;
 
 use Amp\Promise;
 use Amp\Success;
+use Maestro2\Core\Fact\CwdFact;
 use Maestro2\Core\Fact\GroupFact;
 use Maestro2\Core\Filesystem\Filesystem;
 use Maestro2\Core\Report\Report;
@@ -26,19 +27,32 @@ class ReplaceLineHandler implements Handler
     public function run(Task $task, Context $context): Promise
     {
         assert($task instanceof ReplaceLineTask);
+        $this->runReplaceLine(
+            $task,
+            $this->filesystem->cd(
+                $context->factOrNull(CwdFact::class)?->cwd() ?: '/'
+            ),
+            $context->factOrNull(GroupFact::class)?->group() ?: 'replace-line'
+        );
+        
+        return new Success($context);
+    }
+
+    private function runReplaceLine(ReplaceLineTask $task, Filesystem $filesystem, string $group): void
+    {
         if (!$this->filesystem->exists($task->path())) {
             throw new TaskError(sprintf(
                 'File "%s" does not exist',
                 $task->path()
             ));
         }
-
+        
         $before = $this->filesystem->getContents($task->path());
         $after = implode('', array_map(function (string $lineOrDelim) use ($task): string {
             if (preg_match($task->regexp(), $lineOrDelim)) {
                 return $task->line();
             }
-
+        
             return $lineOrDelim;
         }, array_filter((array)preg_split(
             '{(' . self::NEWLINE_PATTERN . ')}',
@@ -46,15 +60,13 @@ class ReplaceLineHandler implements Handler
             -1,
             PREG_SPLIT_DELIM_CAPTURE
         ))));
-
+        
         if ($before !== $after) {
             $this->filesystem->putContents($task->path(), $after);
             $this->publisher->publish(
-                $task->group() ?: $context->fact(GroupFact::class)->group(),
+                $group,
                 Report::ok(sprintf('Replaced line(s) in "%s"', $task->path()))
             );
         }
-
-        return new Success($context);
     }
 }
