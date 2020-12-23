c098b05a85ad80e2c1da995de35e9f2ba0929777
Daniel Leech <dan.t.leech@gmail.com>
2020-12-23 11:29:19 +0000

    Allow version comparisons

--- src/Composer/ComposerVersion.php
+++ src/Composer/ComposerVersion.php
@@ -2,6 +2,7 @@
 
 namespace Maestro\Composer;
 
+use Composer\Semver\Comparator;
 use Stringable;
 
 class ComposerVersion implements Stringable
@@ -16,5 +17,40 @@ class ComposerVersion implements Stringable
     public function __toString(): string
     {
         return $this->version;
+    }
+
+    public function version(): string
+    {
+        return $this->version;
+    }
+
+    public function greaterThan(string $version): bool
+    {
+        return Comparator::greaterThan($this->version, $version);
+    }
+
+    public function greaterThanThanOrEqualTo(string $version): bool
+    {
+        return Comparator::greaterThanOrEqualTo($this->version, $version);
+    }
+
+    public function lessThan(string $version): bool
+    {
+        return Comparator::lessThan($this->version, $version);
+    }
+
+    public function lessThanOrEqualTo(string $version): bool
+    {
+        return Comparator::lessThanOrEqualTo($this->version, $version);
+    }
+
+    public function equalTo(string $version): bool
+    {
+        return Comparator::equalTo($this->version, $version);
+    }
+
+    public function notEqualTo(string $version): bool
+    {
+        return Comparator::notEqualTo($this->version, $version);
     }
 }
